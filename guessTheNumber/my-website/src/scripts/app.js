f0ef207c12d149e6c1cb514a44b2bdb01b43b565
Mattia Spina Agatino <mattiaagatino.spina@gmail.com>
2025-02-24 11:19:08 +0100

    project

---
+++ guessTheNumber/my-website/src/scripts/app.js
@@ -0,0 +1,33 @@
+// crea un numero casuale da 1 a 100
+var numeroCasuale = Math.floor(Math.random() * 100) + 1;
+var input = document.getElementById("guessField");
+var result = document.getElementById("result");
+
+// prendi il numero in input dall'index.html
+function checkGuess() {
+    var guess = parseInt(input.value);
+    if (isNaN(guess)) {
+        result.innerHTML = "ERRORE!!! Inserisci un numero!";
+        result.style.color = "red";
+    } else if (guess < 1 || guess > 100) {
+        result.innerHTML = "ERRORE!!! Il numero deve essere compreso tra 1 e 100!";
+        result.style.color = "red";
+    } else if (guess < numeroCasuale) {
+        result.innerHTML = "Il numero è troppo piccolo!";
+        result.style.color = "lightBlue";
+    } else if (guess > numeroCasuale) {
+        result.innerHTML = "Il numero è troppo grande!";
+        result.style.color = "lightBlue";
+    } else {
+        result.innerHTML = "Hai indovinato!";
+        result.style.color = "lightGreen";
+        input.disabled = true;
+    }
+}
+
+function resetGame() {
+    input.value = "";
+    result.innerHTML = "";
+    input.disabled = false;
+    numeroCasuale = Math.floor(Math.random() * 100) + 1;
+}
