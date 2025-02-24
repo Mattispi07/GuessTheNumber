9dfc24468a24b40092cdac09821d712a56ff8de6
Mattia Spina Agatino <mattiaagatino.spina@gmail.com>
2025-02-24 12:38:40 +0100

    endProject

--- scripts/app.js
+++ scripts/app.js
@@ -3,8 +3,9 @@ var numeroCasuale = Math.floor(Math.random() * 100) + 1;
 var input = document.getElementById("guessField");
 var result = document.getElementById("result");
 var tentativi = document.getElementById("tentativi");
+var bestRecord = document.getElementById("best-record");
 var i = 1; // Inizializza a 1
-
+var best = Infinity;
 // prendi il numero in input dall'index.html
 function checkGuess() {
     var guess = parseInt(input.value);
@@ -17,15 +18,23 @@ function checkGuess() {
     } else if (guess < numeroCasuale) {
         result.innerHTML = "Il numero è troppo piccolo!";
         result.style.color = "lightBlue";
-        tentativi.innerHTML = "Tentativi: " + i++;
+        console.log(i);
+        i += 1;
+        tentativi.innerHTML = "Attempts: " + i;
     } else if (guess > numeroCasuale) {
         result.innerHTML = "Il numero è troppo grande!";
         result.style.color = "lightBlue";
-        tentativi.innerHTML = "Tentativi: " + i++;
+        i += 1;
+        console.log(i);
+        tentativi.innerHTML = "Attempts: " + i;
     } else {
         result.innerHTML = "Hai indovinato!";
         result.style.color = "lightGreen";
         input.disabled = true;
+        if(i < best){
+            best = i;
+            bestRecord.innerHTML = "BestRecord: " + best;
+        }
     }
 }
 
@@ -35,4 +44,5 @@ function resetGame() {
     input.disabled = false;
     numeroCasuale = Math.floor(Math.random() * 100) + 1;
     i = 1; // Resetta il conteggio dei tentativi
+    tentativi.innerHTML = "Tentativi: " + i;
 }
